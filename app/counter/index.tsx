152468ce9b8c6e6c1a9b0de3df7799265748554c
Aion.Witstorm@gmail.com <Aion.Witstorm@gmail.com>
2026-01-29 03:13:54 +0545

    Add local push notifications

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,9 +1,57 @@
-import { StyleSheet, Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { theme } from "../../theme";
+import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
 
 export default function CounterScreen() {
+  // ✅ Just for Foreground notifications handler
+  useEffect(() => {
+    (async () => {
+      const Notifications = await import("expo-notifications");
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldPlaySound: true,
+          shouldSetBadge: true,
+          shouldShowBanner: true,
+          shouldShowList: true,
+        }),
+      });
+    })();
+  }, []);
+
+  const scheduleNotification = async () => {
+    const result = await registerForPushNotificationsAsync();
+    //  console.log("Permission: ", result);
+    if (result === "granted") {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "I'm a notification from your app! 📨",
+          body: "It should pop up in 5s",
+          sound: "default",
+        },
+        trigger: {
+          type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+          seconds: 5,
+        },
+      });
+    } else {
+      Alert.alert(
+        "Unable to schedule notification",
+        "Enable the notifications permission for Expo Go in settings",
+      );
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Counter</Text>
+      <TouchableOpacity
+        onPress={scheduleNotification}
+        style={styles.button}
+        activeOpacity={0.8}
+      >
+        <Text style={styles.buttonText}>Schedule notification</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,7 +62,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  text: {
-    fontSize: 24,
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 12,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
 });
